700720e4ad00deeb6f78f031a535337d6aa95c5f
Ajilenakh <107887918+ajilenakh@users.noreply.github.com>
2024-11-15 23:40:32 +0530

    frontend for filtring resolution done

--- frontend/scripts/script.js
+++ frontend/scripts/script.js
@@ -1,4 +1,3 @@
-
 const api_address = "https://0.0.0.0:8000";
 
     function getResolutions() {
@@ -12,17 +11,49 @@ const api_address = "https://0.0.0.0:8000";
 
         // Send the GET request to the API endpoint
         fetch(`${api_address}/filter_resolution/?url=${InputUrl}`)
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            if (!Array.isArray(data)){
+                throw new Error('Expected an array of items in the response');
+            }
+
+            const items = data;
+            const listContainer = document.getElementById('responseList');
+
+            // Clean the previous content in the list
+            listContainer.innerHTML = '';
+
+            // Looping through each item and create the list
+            items.forEach(item => {
+                if (item.format_note && item.highest_filesize) {
+                    const notes = item.format_note;
+                    const filesize = (item.highest_filesize / 1000000).toFixed(2);
+
+                    if (/\p/.test(item.format_note)) {
+                        const listItem = document.createElement('li');
+                        listItem.textContent = `${notes} | ${filesize} MB`;
+
+                        listItem.addEventListener('click', () => {
+                            //yet to add the function to prepare download
+                        });
+                    
+                        listContainer.appendChild(listItem);
+                    }
                 }
-                return response.json();
-            })
-            .then(data => {
-                console.log(data);  // Process the API response data
-            })
-            .catch(error => {
-                console.error('There was a problem with the fetch operation:', error);
-                alert("There was an error fetching the resolutions.");
             });
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+            const listContainer = document.getElementById('responseList');
+            listContainer.innerHTML = `Error: ${error.message}`;
+        });
     }
+
+function postVideoRequest(url,format_id){
+
+}
